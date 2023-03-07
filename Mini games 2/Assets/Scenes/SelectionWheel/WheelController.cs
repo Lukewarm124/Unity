e97222af40708ed5f3f0adf9b438f75029ce415c
Lukewarm124 <115736288+Lukewarm124@users.noreply.github.com>
2023-03-07 13:03:41 -0500

    Worked on the wheel rotation

--- Mini games 2/Assets/Scenes/SelectionWheel/WheelController.cs
+++ Mini games 2/Assets/Scenes/SelectionWheel/WheelController.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WheelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int wheelRotation;
+    public GameObject wheel;
+    public GameObject wheelSection1;
+    public GameObject wheelSection2;
+    public GameObject wheelSection3;
+    public GameObject wheelSection4;
+    public GameObject wheelSection5;
+    public GameObject wheelSection6;
+    public GameObject wheelSection7;
+    public GameObject wheelSection8;
+    public GameObject selectorBit;
+    private List<int> gameList = new List<int>();
+    private Boolean wheelSpinRest=true;
+
+    private void Start()
     {
-        
+        wheelRotation = UnityEngine.Random.Range(0, 9);
+        wheelRotation = (wheelRotation * 45) + (360 * 5);
+
     }
 
-    // Update is called once per frame
+    private void spinWheel()
+    {
+        print(wheelRotation);
+        wheelSpinRest = false;
+        wheel.transform.Rotate(0, 0, 1);
+        wheelRotation -= 1;
+        Invoke("wheelSpinRestTrue", .01f);
+    }
+    private void wheelSpinRestTrue()
+    {
+        wheelSpinRest=true;
+    }
     void Update()
     {
-        
+        if(wheelSpinRest &&  wheelRotation > 0)
+        {
+            spinWheel();
+        }
     }
 }
