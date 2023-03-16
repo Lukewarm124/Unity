0f5e1bbbf27168e7ac1c875ad2eacb3fdf82b8a0
Lukewarm124 <115736288+Lukewarm124@users.noreply.github.com>
2023-03-16 08:22:08 -0400

    Revert "Revert "finished color game started transition""
    This reverts commit f35dd9d5836f8b8ea9828d7d1dcc8bc58f051726.

--- Mini games 2/Assets/Scenes/ColorSwitch/PlatformOperator.cs
+++ Mini games 2/Assets/Scenes/ColorSwitch/PlatformOperator.cs
@@ -7,7 +7,14 @@ public class PlatformOperator : MonoBehaviour
 {
     private bool gameOn = true;
     public bool roundOn = false;
+    private bool gameWinner = false;
     public float timerVal;
+    public int lastBlock;
+
+    public GameObject P1;
+    public GameObject P2;
+    public GameObject gameCamera;
+
     public GameObject lightsBL;
     public GameObject lightsBR;
     public GameObject lightsTL;
@@ -16,6 +23,7 @@ public class PlatformOperator : MonoBehaviour
     public GameObject BR;
     public GameObject TL;
     public GameObject TR;
+
     public GameObject chosenBlock;
     public GameObject chosenLight;
 
@@ -23,14 +31,34 @@ public class PlatformOperator : MonoBehaviour
     {
         startNewRound();
     }
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if (!(roundOn) && gameOn)
         {
             startNewRound();
         }
+        if (P1.transform.position.y < 0)
+        {
+            gameWinner = false;
+            activateEndScreen();
+        }
+        else if (P2.transform.position.y < 0)
+        {
+            gameWinner = true;
+            activateEndScreen();
+        }
     }
-
+    void activateEndScreen()
+    {
+        if (gameWinner)
+        {
+            gameCamera.transform.Find("Player1WinScreen").gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            gameCamera.transform.Find("Player2WinScreen").gameObject.GetComponent<Renderer>().enabled = true;
+        }
+    }
     private void startNewRound()
     {
         roundOn= true;
@@ -39,37 +67,50 @@ public class PlatformOperator : MonoBehaviour
         activateLight();
         Invoke("activateBlock",timerVal);
     }
-
     private void startTimer()
     {
-        timerVal *= .3f;
+        timerVal *= .8f;
     }
-
-
     private void choosePlatform()
     {
-        int block = (UnityEngine.Random.Range(1,5));
+        int block;
+        while (true)
+        {
+            block = getRandNum();
+            print(block);
+            if (block != lastBlock)
+            {
+                break;
+            }
+        }
         if (block == 1)
         {
+            lastBlock = 1;
             chosenBlock = BL;
             chosenLight = lightsBL;
         }
-        else if(block == 2) 
+        else if(block == 2)
         {
+            lastBlock = 2;
             chosenBlock = BR;
             chosenLight = lightsBR;
         }
         else if (block == 3)
         {
+            lastBlock = 3;
             chosenBlock = TL;
             chosenLight = lightsTL;
         }
         else if (block == 4)
         {
+            lastBlock = 4;
             chosenBlock = TR;
             chosenLight = lightsTR;
-        }
-        chosenBlock = null;    
+        } 
+    }
+    private int getRandNum()
+    {
+        return (UnityEngine.Random.Range(1, 5));
     }
     private void activateLight()
     {
@@ -77,8 +118,8 @@ public class PlatformOperator : MonoBehaviour
         lightsBR.GetComponent<Renderer>().enabled = false;
         lightsTL.GetComponent<Renderer>().enabled = false;
         lightsTR.GetComponent<Renderer>().enabled = false;
+        chosenLight.GetComponent<Renderer>().enabled = true;
     }
-
     private void activateBlock()
     {
         lowerBlock(BL);
@@ -87,11 +128,11 @@ public class PlatformOperator : MonoBehaviour
         lowerBlock(TR);
 
         raiseBlock(chosenBlock);
-        Invoke("raiseAllBlocks", 2);
+        Invoke("raiseAllBlocks", 3);
     }
     private void lowerBlock(GameObject block)
     {
-        block.transform.position = new Vector3(block.transform.position.x, -3, block.transform.position.z);
+        block.transform.position = new Vector3(block.transform.position.x, -7, block.transform.position.z);
     }
     private void raiseBlock(GameObject block)
     {
@@ -103,7 +144,7 @@ public class PlatformOperator : MonoBehaviour
         BL.transform.position = new Vector3(BL.transform.position.x, 0, BL.transform.position.z);
         TL.transform.position = new Vector3(TL.transform.position.x, 0, TL.transform.position.z);
         TR.transform.position = new Vector3(TR.transform.position.x, 0, TR.transform.position.z);
-
+        roundOn = false;
     }
 
 }
---
+++ Mini games 2/Assets/Scenes/TransitionScreen/TransitionOperator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionOperator : MonoBehaviour
+{
+    public GameObject wheelOp;
+    private int nextGame;
+    void Update()
+    {
+        
+    }
+}
